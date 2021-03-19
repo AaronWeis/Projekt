3d0ef09449c3c2e66b7aa888d4363179c8c4e96a
AaronWeis <tuba.aaron@gmail.com>
2021-03-19 12:23:14 +0100

    added new Class Craftsman and new get-methods in Profession.java

---
+++ src/Craftsman.java
@@ -0,0 +1,14 @@
+public class Craftsman extends Profession{
+
+    public Craftsman(){
+        loan = 3500;
+        work_time[0] =  8.5;
+        work_time[1] =  16.5;
+    }
+    @Override
+    public void work() {
+
+    }
+
+
+}
--- src/Profession.java
+++ src/Profession.java
@@ -1,5 +1,19 @@
 public abstract class Profession {
     public int loan;
-    public float[] work_time = new float[2];
+    public double[] work_time = new double[2];
     public abstract void work();
+    public int get_loan(){
+        return loan;
+    }
+    public double get_work_time(String time){
+        switch(time){
+            case "Begin":
+                return work_time[0];
+            case "End":
+                return work_time[1];
+            case "Duration":
+                return work_time[1] - work_time[0];
+        }
+        return -1.0;
+    }
 }
