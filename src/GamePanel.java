a09371d1c36c2d789f80213eacf1631a7967356a
Leto <leto-ziegler@web.de>
2021-04-02 22:15:00 +0200

    Mausadapter changes: MouseListener => Mouseadapter
    --> MouseDragged um die map zu bewegen, wird später eupdatet, Gerüst ledeglich hinzugefügt

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,8 +5,10 @@
  */
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseWheelEvent;
 
 /**
  *
@@ -84,6 +86,47 @@ public class GamePanel extends javax.swing.JPanel {
             @Override
             public void mouseExited(MouseEvent e) {
 
+            }
+        });
+        this.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+            }
+
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                super.mouseWheelMoved(e);
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                super.mouseDragged(e);
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
             }
         });
         ctr = new Control();
